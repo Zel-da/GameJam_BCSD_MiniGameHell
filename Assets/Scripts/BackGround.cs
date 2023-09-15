3610da33c08e6ca6461fa2b8e6c2293abf31a5f6
Zel-da <anjongman1029@gmail.com>
2023-09-15 23:55:39 +0900

    Game1

--- Assets/Scripts/BackGround.cs
+++ Assets/Scripts/BackGround.cs
@@ -11,12 +11,19 @@ public class BackGround : MonoBehaviour
     void Start()
     {
         render = GetComponent<MeshRenderer>();
+        // 10초마다 IncreaseSpeed 함수를 호출합니다.
+        InvokeRepeating("IncreaseSpeed", 10f, 10f);
     }
 
     void Update()
     {
-
         offset += Time.deltaTime * speed;
         render.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+
+    void IncreaseSpeed()
+    {
+        // speed를 0.1씩 증가시킵니다.
+        speed += 0.05f;
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText; // 점수를 표시할 Text 엘리먼트
+    public float scoreIncreaseInterval = 0.1f; // 점수를 증가시키는 주기 (예: 1초마다)
+    public int scoreIncreaseAmount = 10; // 점수가 증가하는 양
+
+    private int score = 0;
+    private float timeSinceLastIncrease = 0.0f;
+    private bool isScoreStopped = false;
+
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    void Update()
+    {
+        if (!isScoreStopped) // 점수 증가 중지되지 않았을 때만 증가
+        {
+            // 경과한 시간을 누적
+            timeSinceLastIncrease += Time.deltaTime;
+
+            // scoreIncreaseInterval 간격마다 점수를 증가
+            if (timeSinceLastIncrease >= scoreIncreaseInterval)
+            {
+                IncreaseScore(scoreIncreaseAmount);
+                timeSinceLastIncrease = 0.0f; // 시간 초기화
+            }
+        }
+    }
+
+        // 점수를 증가시키는 메소드
+        public void IncreaseScore(int amount)
+    {
+        score += amount;
+        UpdateScoreText();
+    }
+
+    // Text 엘리먼트에 현재 점수를 업데이트하는 메소드
+    void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void StopScore()
+    {
+        isScoreStopped = true;
+    }
+}
